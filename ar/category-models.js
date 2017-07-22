f04e64554fe7d378032d00ee49356e25ce73752e
kadotami <mondamin.kqfb@gmail.com>
2017-07-22 16:50:25 +0900

    aaa

--- ar/category-models.js
+++ ar/category-models.js
@@ -6,8 +6,8 @@ category_models = {
         guiter.load("model/guitar.json", function(geo, mat) {　　　
             var faceMat = new THREE.MeshFaceMaterial(mat);
             var model = new THREE.Mesh(geo, faceMat);
-            model.position.set(0.3, 0.3, 0);　　
-            model.rotation.set(0, 0, Math.PI/4);　
+            model.position.set(-0.3, 0.3, 0);　　
+            model.rotation.set(0, 0, -Math.PI/4);　
             model.scale.set(0.5, 0.5, 0.5);　　　
             scene.add(model);　　　
         });
