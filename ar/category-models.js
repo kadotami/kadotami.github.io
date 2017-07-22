cd5d935773d07cf0176a9250a2e618c9da20c4fe
kadotami <mondamin.kqfb@gmail.com>
2017-07-22 15:46:39 +0900

    aaa

---
+++ ar/category-models.js
@@ -0,0 +1,22 @@
+var category_models = {};    
+    
+category_models = {
+    "music_model": function(scene) {
+        var guiter = new THREE.JSONLoader();　　
+        guiter.load("model/guitar.json", function(geo, mat) {　　　
+            var faceMat = new THREE.MeshFaceMaterial(mat);
+            var model = new THREE.Mesh(geo, faceMat);
+            model.position.set(0, 0, 0);　　　
+            model.scale.set(0.5, 0.5, 0.5);　　　
+            scene.add(model);　　　
+        });
+        var piano = new THREE.JSONLoader();　　
+        piano.load("model/piano.json", function(geo, mat) {　　　
+            var faceMat = new THREE.MeshFaceMaterial(mat);
+            var model = new THREE.Mesh(geo, faceMat);
+            model.position.set(0, 0, 0);　　　
+            model.scale.set(0.5, 0.5, 0.5);　　　
+            scene.add(model);　　　
+        });
+    }
+}
