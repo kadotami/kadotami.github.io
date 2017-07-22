cba9009dd2244e140a7a48d8bd88c13db0faafb8
kadotami <mondamin.kqfb@gmail.com>
2017-07-22 22:32:04 +0900

    3d調整

--- ar/category-models.js
+++ ar/category-models.js
@@ -3,39 +3,30 @@ var category_models = {};
 category_models = {
     "music_model": function(scene) {
         var guiter = new THREE.JSONLoader();　　
-        guiter.load("model/guitar.json", function(geo, mat) {　　　
+        guiter.load("model/music/guitar.json", function(geo, mat) {　　　
             var faceMat = new THREE.MeshFaceMaterial(mat);
             var model = new THREE.Mesh(geo, faceMat);
-            model.position.set(-0.3, 0, -0.2);
+            model.position.set(-0.5, 0, -0.1);
             model.rotation.set(0, 0, Math.PI*7/4);
             model.scale.set(0.3, 0.3, 0.3);
             scene.add(model);　　　
         });
         var piano = new THREE.JSONLoader();　　
-        piano.load("model/piano.json", function(geo, mat) {　　　
+        piano.load("model/music/piano.json", function(geo, mat) {　　　
             var faceMat = new THREE.MeshFaceMaterial(mat);
             var model = new THREE.Mesh(geo, faceMat);
             model.position.set(0, 0, 0);
             model.scale.set(0.3, 0.3, 0.3);
             scene.add(model);　　　
         });
-        var loader = new THREE.FontLoader();
-        loader.load( 'fonts/helvetiker_bold.typeface.json', function (font) {
-            var textGeometry = new THREE.TextGeometry( 'MY HOBBY IS MUSIC!!!', {
-                font: font,
-                size: 10.0,
-                height: 5,
-                curveSegments: 10,
-                bevelThickness: 3,
-                bevelSize: 1.0,
-                bevelEnabled: true
-            });
-            var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );    
-            var text = new THREE.Mesh(textGeometry, material);
-            model.position.set(0, 0, 0);
-            model.scale.set(1, 1, 1);  
-            scene.add( text );
-        })
+        var text = new THREE.JSONLoader();　　
+        text.load("model/music/text.json", function(geo, mat) {　　　
+            var faceMat = new THREE.MeshFaceMaterial(mat);
+            var model = new THREE.Mesh(geo, faceMat);
+            model.position.set(0, 0, 0.2);
+            model.scale.set(0.3, 0.3, 0.3);
+            scene.add(model);　　　
+        });
     },
 
     "gourmet_model": function(scene) {
@@ -47,8 +38,16 @@ category_models = {
         soccer.load("model/soccerball.json", function(geo, mat) {　　　
             var faceMat = new THREE.MeshFaceMaterial(mat);
             var model = new THREE.Mesh(geo, faceMat);
-            model.position.set(0, 0, 0);　　　
-            model.scale.set(0.3, 0.3, 0.3);　　　
+            model.position.set(-0.2, 0, 0);　　　
+            model.scale.set(0.5, 0.5, 0.5);　　　
+            scene.add(model);　　　
+        });
+        var baseball = new THREE.JSONLoader();　　
+        baseball.load("model/baseball.json", function(geo, mat) {　　　
+            var faceMat = new THREE.MeshFaceMaterial(mat);
+            var model = new THREE.Mesh(geo, faceMat);
+            model.position.set(0., 0, 0);　　　
+            model.scale.set(0.5, 0.5, 0.5);　　　
             scene.add(model);　　　
         });
     },
@@ -56,6 +55,17 @@ category_models = {
     "game_model": function(scene) {
         var game = new THREE.JSONLoader();　　
         game.load("model/game.json", function(geo, mat) {　　　
+            var faceMat = new THREE.MeshFaceMaterial(mat);
+            var model = new THREE.Mesh(geo, faceMat);
+            model.position.set(0, 0, 0);　　　
+            model.scale.set(0.3, 0.3, 0.3);　　　
+            scene.add(model);　　　
+        });
+    },
+
+    "trip_model": function(scene) {
+        var plane = new THREE.JSONLoader();　　
+        plane.load("model/plane.json", function(geo, mat) {　　　
             var faceMat = new THREE.MeshFaceMaterial(mat);
             var model = new THREE.Mesh(geo, faceMat);
             model.position.set(0, 0, 0);　　　
