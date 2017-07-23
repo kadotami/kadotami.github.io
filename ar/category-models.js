7e58715909362b276cb0ded1a608b64bd258dabc
kadotami <mondamin.kqfb@gmail.com>
2017-07-23 17:39:47 +0900

    3d調整

--- ar/category-models.js
+++ ar/category-models.js
@@ -3,30 +3,38 @@ var category_models = {};
 category_models = {
     "music_model": function(scene) {
         var guiter = new THREE.JSONLoader();　　
-        guiter.load("model/music/guitar.json", function(geo, mat) {　　　
-            var faceMat = new THREE.MeshFaceMaterial(mat);
-            var model = new THREE.Mesh(geo, faceMat);
-            model.position.set(-0.5, 0, -0.1);
-            model.rotation.set(0, 0, Math.PI*7/4);
-            model.scale.set(0.3, 0.3, 0.3);
-            scene.add(model);　　　
-        });
-        var piano = new THREE.JSONLoader();　　
-        piano.load("model/music/piano.json", function(geo, mat) {　　　
+        guiter.load("model/music/music.json", function(geo, mat) {　　　
             var faceMat = new THREE.MeshFaceMaterial(mat);
             var model = new THREE.Mesh(geo, faceMat);
             model.position.set(0, 0, 0);
             model.scale.set(0.3, 0.3, 0.3);
             scene.add(model);　　　
         });
-        var text = new THREE.JSONLoader();　　
-        text.load("model/music/text.json", function(geo, mat) {　　　
-            var faceMat = new THREE.MeshFaceMaterial(mat);
-            var model = new THREE.Mesh(geo, faceMat);
-            model.position.set(0, 0, 1.0);
-            model.scale.set(0.3, 0.3, 0.3);
-            scene.add(model);　　　
-        });
+        // var guiter = new THREE.JSONLoader();　　
+        // guiter.load("model/music/guitar.json", function(geo, mat) {　　　
+        //     var faceMat = new THREE.MeshFaceMaterial(mat);
+        //     var model = new THREE.Mesh(geo, faceMat);
+        //     model.position.set(-0.5, 0, -0.1);
+        //     model.rotation.set(0, 0, Math.PI*7/4);
+        //     model.scale.set(0.3, 0.3, 0.3);
+        //     scene.add(model);　　　
+        // });
+        // var piano = new THREE.JSONLoader();　　
+        // piano.load("model/music/piano.json", function(geo, mat) {　　　
+        //     var faceMat = new THREE.MeshFaceMaterial(mat);
+        //     var model = new THREE.Mesh(geo, faceMat);
+        //     model.position.set(0, 0, 0);
+        //     model.scale.set(0.3, 0.3, 0.3);
+        //     scene.add(model);　　　
+        // });
+        // var text = new THREE.JSONLoader();　　
+        // text.load("model/music/text.json", function(geo, mat) {　　　
+        //     var faceMat = new THREE.MeshFaceMaterial(mat);
+        //     var model = new THREE.Mesh(geo, faceMat);
+        //     model.position.set(0, 0, 1.0);
+        //     model.scale.set(0.3, 0.3, 0.3);
+        //     scene.add(model);　　　
+        // });
     },
 
     "gourmet_model": function(scene) {
